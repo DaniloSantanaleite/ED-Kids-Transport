be90996ac9a52f0cfb5913249d1626cde85e3218
DaniloSantanleite <danilo.baumann@icloud.com>
2025-01-16 10:40:54 -0300

    Ajustes Iframe e JS

---
+++ js/index.js
@@ -0,0 +1,14 @@
+const optionOne = document.getElementById ("firstOption")
+
+const changeColor = () =>{
+    optionOne.style.color = " #507D5D"
+    optionOne.style.fontWeight = "Bold"
+
+    
+}
+
+optionOne.addEventListener("click", changeColor)
+
+
+
+
