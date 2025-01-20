34dc2e3e12023fa21b7edac5325694e3d9146be9
DaniloSantanleite <danilo.baumann@icloud.com>
2025-01-20 11:01:42 -0300

    Ajuste no Btn de voltar

--- js/index.js
+++ js/index.js
@@ -1,18 +1,19 @@
+
 const optionOne = document.getElementById ("firstOption")
 const square = document.getElementById ("magicSquare")
-const btnvolta = document.getElementById("backbtn")
+
+
 
 const changeSquare = () =>{
      square.src = "../html/perfilpg.html"  
 }
 
-const clickAndBack = () => {
-     square.src = "../html/welcomepg.html"
-}
-
-
 optionOne.addEventListener("click", changeSquare)
-btnvolta.addEventListener("click",clickAndBack)
+
+
+
+
+
 
 
 
