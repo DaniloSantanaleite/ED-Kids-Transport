7ccd7d88967014c9e5f0dddef89ae3927960cf52
DaniloSantanleite <danilo.baumann@icloud.com>
2025-01-20 09:43:23 -0300

    Tentando conectar mais de uma pg html a um único JS

--- js/index.js
+++ js/index.js
@@ -1,12 +1,18 @@
 const optionOne = document.getElementById ("firstOption")
 const square = document.getElementById ("magicSquare")
+const btnvolta = document.getElementById("backbtn")
 
 const changeSquare = () =>{
-     square.src = "../html/index1.html"
-    
+     square.src = "../html/perfilpg.html"  
 }
 
+const clickAndBack = () => {
+     square.src = "../html/welcomepg.html"
+}
+
+
 optionOne.addEventListener("click", changeSquare)
+btnvolta.addEventListener("click",clickAndBack)
 
 
 
